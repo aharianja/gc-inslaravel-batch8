c8da4e7ec712a4c4182ffc8df5f2d98b3621346e
yudipradana <aharianja051@gmail.com>
2023-11-25 13:49:48 +0700

    Tugas materi 12 saya

---
+++ Tugas12/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Selamat Datang || <i>Ahlan'wa sahlan</i> || <i>Welcome</i> || <i>Sugeng Rawuh</i> || <i>Wilujeng Sumping</i> </h1>
+    <h3><a href="/register">Daftar</a><h/3>
+</body>
+</html>
